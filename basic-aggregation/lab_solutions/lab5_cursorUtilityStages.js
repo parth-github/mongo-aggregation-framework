9f679ed2e46d27c427c6d7af004898b46d36c83e
KUNAL KR <nkunal@aol.com>
2021-06-18 17:34:13 +0530

    lab5 utility stages example updated - $addFields

--- basic-aggregation/lab_solutions/lab5_cursorUtilityStages.js
+++ basic-aggregation/lab_solutions/lab5_cursorUtilityStages.js
@@ -43,5 +43,47 @@ var pipeline = [{
     }
 ]
 
+// NOTE: Above pipeline can be written by adding $addFields stages to compute 'num_favs'.
+
+var pipeline = [{
+        "$match": {
+            "tomatoes.viewer.rating": {
+                "$gte": 3
+            },
+            "countries": "USA",
+            "cast": {
+                "$in": favorites
+            }
+        }
+    }, {
+        "$addFields": {
+            "num_favs": {
+                "$size": {
+                    "$setIntersection": ["$cast", favorites]
+                }
+            }
+        }
+    }, {
+        "$project": {
+            "_id": 0,
+            "title": 1,
+            "tomatoes.viewer.rating": 1,
+            "num_favs": 1
+        }
+    }, {
+        "$sort": {
+            "num_favs": -1,
+            "tomatoes.viewer.rating": -1,
+            "title": -1
+        }
+    }, {
+        "$skip": 24
+    }, {
+        "$limit": 1
+    }
+]
+
+
 // Aggregate the pipeline stages and pretty() print the output
 db.movies.aggregate(pipeline).pretty()
+
