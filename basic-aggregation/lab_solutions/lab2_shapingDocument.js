e92177b8f3ce65b334a30ca6a2c6e5d6dcd1a379
KUNAL KR <nkunal@aol.com>
2021-06-19 20:01:52 +0530

    Add more examples in lab2 related to $group stages - $concat,$toUpper,$substrCP,$strLenCP,$subtract.Updated Readme.md file to add more information related to project structure.

--- basic-aggregation/lab_solutions/lab2_shapingDocument.js
+++ basic-aggregation/lab_solutions/lab2_shapingDocument.js
@@ -1,5 +1,5 @@
 /* Basic Aggregation - $match and $project
-Lab - $project
+Solution for PROBLEM #1 - $project
 */
 
 var pipeline = [{
@@ -41,3 +41,100 @@ load('<your-directory-path>\\validateLab2.js')
 // Run validateLab1 validation method
 
 validateLab2(pipeline)
+
+
+
+/* Basic Aggregation - $match and $project
+Solution for PROBLEM #2 - $project, $toUpper
+*/
+
+var pipeline = [{
+        "$project": {
+            "_id": 0,
+            "gender": 1,
+            "Full Name": {
+                "$toUpper": {
+                    "$concat": ["$name.title", " ", "$name.first", " ", "$name.last"]
+                }
+            }
+        }
+    }
+]
+
+
+OR,
+
+var pipeline = [{
+        "$project": {
+            "_id": 0,
+            "gender": 1,
+            "Full Name": {
+                "$concat": [{
+                        "$toUpper": "$name.title"
+                    }, " ", {
+                        "$toUpper": "$name.first"
+                    }, " ", {
+                        "$toUpper": "$name.last"
+                    }
+                ]
+            }
+        }
+    }
+]
+
+
+/* Basic Aggregation - $match and $project
+Solution for PROBLEM #3 - $project, $concat, $strLenCP, $substrCP, $subtract
+*/
+
+var pipeline = [{
+        "$project": {
+            "_id": 0,
+            "gender": 1,
+            "Full Name": {
+                "$concat": [{
+                        "$toUpper": {
+                            "$substrCP": ["$name.title", 0, 1]
+                        }
+                    }, {
+                        "$substrCP": ["$name.title", 1, {
+                                "$subtract": [{
+                                        "$strLenCP": "$name.title"
+                                    }, 1]
+                            }
+                        ]
+                    }, " ", {
+                        "$toUpper": {
+                            "$substrCP": ["$name.first", 0, 1]
+                        }
+                    }, {
+                        "$substrCP": ["$name.first", 1, {
+                                "$subtract": [{
+                                        "$strLenCP": "$name.first"
+                                    }, 1]
+                            }
+                        ]
+                    }, " ", {
+                        "$toUpper": {
+                            "$substrCP": ["$name.last", 0, 1]
+                        }
+                    }, {
+                        "$substrCP": ["$name.last", 1, {
+                                "$subtract": [{
+                                        "$strLenCP": "$name.last"
+                                    }, 1]
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+]
+
+
+// Calculate aggregate values for the data in a collection - 'persons'
+db.persons.aggregate(pipeline).pretty()
+
+//Counts the number of documents remaining in a cursor.
+db.persons.aggregate(pipeline).itcount()
