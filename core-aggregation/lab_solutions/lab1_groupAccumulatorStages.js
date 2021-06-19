2e74d2c2c52dc3f326dd38fa9ddb82b50f59ca19
KUNAL KR <nkunal@aol.com>
2021-06-19 14:06:52 +0530

    moved $group stage computation examples(text,js files) from basic-aggregation labs to core-aggregation labs

--- core-aggregation/lab_solutions/lab1_groupAccumulatorStages.js
+++ core-aggregation/lab_solutions/lab1_groupAccumulatorStages.js
@@ -1,5 +1,5 @@
 /* Core Aggregation - Combining Information
-Lab - $group and Accumulators
+Solution for PROBLEM #1 - $group and Accumulators
 */
 
 var pipeline = [{
@@ -33,4 +33,43 @@ var pipeline = [{
 
 // Aggregate the pipeline stages and pretty() print the output
 db.movies.aggregate(pipeline).pretty()
+
+
+/* Core Aggregation - Combining Information
+Solution for PROBLEM #2 - $group and Accumulators
+*/
+
+var pipeline = [{
+        "$match": {
+            "dob.age": {
+                "$gt": 50
+            }
+        }
+    }, {
+        "$group": {
+            "_id": {
+                "gender": "$gender"
+            },
+            "totalCount": {
+                "$sum": 1
+            },
+            "averageAge": {
+                "$avg": "$dob.age"
+            }
+        }
+    }, {
+        "$sort": {
+            "totalCount": -1
+        }
+    }
+]
+
+
+// Calculate aggregate values for the data in a collection 'persons'
+
+db.persons.aggregate(pipeline).pretty()
+
+//Counts the number of documents remaining in a cursor.
+
+db.persons.aggregate(pipeline).itcount()
 
